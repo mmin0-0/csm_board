af1e83f5e235d5e64bf2cccfad62a4663ecab66d
Choi minyoung <84059199+mmin0-0@users.noreply.github.com>
2025-03-17 12:20:26 +0900

    setting

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head />
+      <title>CSM</title>
+      <meta charSet="utf-8"></meta>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
